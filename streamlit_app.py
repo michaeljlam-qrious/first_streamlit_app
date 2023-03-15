872116ab7f5265e8bc889627c73b44dc53878a1b
michaeljlam-qrious <127819737+michaeljlam-qrious@users.noreply.github.com>
2023-03-15 17:50:07 +1300

    Update streamlit_app.py
    Add user input text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,10 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Convert the json text to tabular format  
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
